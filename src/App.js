374b3e63d45a895910e648e4d356786857641703
yanntrc <yann.troadec.5@gmail.com>
2021-09-10 13:28:14 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Showcase from './components/Showcase'
+import Projects from './components/Projects'
+import Hire from './components/Hire'
+
+function App() {
+
+  return (
+    <Router>
+       <Header />
+       <Route path="/" exact> 
+       <Showcase />
+       </Route>
+       <Route path="/projects"> 
+       <Projects />
+       </Route>
+       <Route path="/hire"> 
+       <Hire />
+       </Route>
+    </Router>
+  );
+}
+
+export default App;
