50c7a5e062b75ccdf2c699aa648c9ff0e4c04779
yanntrc <yann.troadec.5@gmail.com>
2021-09-10 13:30:24 +0200

    Spelling

--- src/components/Hire.js
+++ src/components/Hire.js
@@ -7,13 +7,13 @@ const Hire = () => {
              <div className="overlay flex flex-col items-center justify-center ">
                <div className="flex flex-col w-9/12 lg:w-auto  lg:flex-row">
                <div className="bg-green-500 py-10 rounded lg:px-0 lg:pt-32">
-               <h2 className="text-white text-center text-5xl font-semibold mb-5 lg:text-7xl">hire a dev</h2>
-               <p className="text-white text-center text-md  font-semibold mb-5 lg:text-4xl">replies within 24 hours </p>
+               <h2 className="text-white text-center text-5xl font-semibold mb-5 lg:text-7xl">Hire a dev</h2>
+               <p className="text-white text-center text-md  font-semibold mb-5 lg:text-4xl">Replies within 24 hours </p>
            </div>
  
            <form name="feedback form" method= "post" className="flex flex-col bg-green-200 py-5 px-10 rounded lg:w-7/12"> 
-               <input type="text" name="username" id="username" placeholder="enter your username" required className="py-2 px-4 mb-5 rounded border border-solid border-green-500 text-green-500 placeholder-green-500 font-semibold"/>
-               <input name="email" type="email" id="email" placeholder="enter your email" required className="py-2 px-4 mb-5 rounded border border-solid border-green-500 text-green-500 placeholder-green-500 font-semibold"/>
+               <input type="text" name="username" id="username" placeholder="Enter your username" required className="py-2 px-4 mb-5 rounded border border-solid border-green-500 text-green-500 placeholder-green-500 font-semibold"/>
+               <input name="email" type="email" id="email" placeholder="Enter your email" required className="py-2 px-4 mb-5 rounded border border-solid border-green-500 text-green-500 placeholder-green-500 font-semibold"/>
                <textarea name="textarea" id="textarea" cols="30" rows="10" placeholder="Enter message" required className="py-2 px-4 mb-5 rounded border border-solid border-green-500 text-green-500 placeholder-green-500 font-semibold"></textarea>
                <input type="submit" value="submit" className="bg-green-500 text-white font-bold tracking-wider py-2 rounded cursor-pointer transition-all hover:bg-green-800"/>
            </form>
